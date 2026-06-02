93989079196e2d5eda2bbea44f24a17dfd95504b
Luis Ortega <felipeortegatacares@gmail.com>
2026-06-02 13:32:16 -0600

    Initial commit

---
+++ src/main/java/com/example/MoneyMind/MoneyMindApplication.java
@@ -0,0 +1,13 @@
+package com.example.MoneyMind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyMindApplication.class, args);
+	}
+
+}
