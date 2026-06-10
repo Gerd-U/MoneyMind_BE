01c168f26478e01fa61bd44775c0b20151c92fed
Ana Cecilia Gonzalez <silva05anagonzalez@gmail.com>
2026-06-09 19:49:14 -0600

    feat: add CorsConfig and application-local.properties

--- src/main/java/com/example/MoneyMind/MoneyMindApplication.java
+++ src/main/java/com/example/MoneyMind/MoneyMindApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MoneyMindApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MoneyMindApplication.class, args);
 	}
